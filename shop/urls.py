d3f616b7cd8a4adf525a53ccda1d1d3017ff3324
LianaSl <sljonkina@gmail.com>
2026-08-09 21:22:38 +0200

    update

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path, include
-from .views import home
+from .views import home, products, about_us
 
 urlpatterns = [
- path('home/', home, name='home_page')
+ path("",'home/', home, name='home_page'),
+ path('products/', products, name='products_page'),
+ path('about_us/', about_us, name='about_us_page'),
  
 ]
--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("Welcome to the shop!")
+
+def products(request):
+    return HttpResponse("Here are our products!")
+
+def about_us(request):
+    return HttpResponse("About us page.")
+def hello(request, name):
+    return HttpResponse(f"Hello, {name}!")
