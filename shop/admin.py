4a80199db2d5f85019d6a169f62d231bb0ce6a2e
LianaSl <sljonkina@gmail.com>
2026-08-23 21:39:35 +0200

    update

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Products
+
+class AdminProducts(admin.ModelAdmin):
+    list_display = ('name', 'price', 'is_stock')
+    search_fields = ('name', 'description')
+
+admin.site.register(Products)
 
 # Register your models here.
